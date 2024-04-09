08ccf0563db66feb943301234927ddd059e724e3
Temirlan <ynwredd@mail.ru>
2024-04-10 03:27:25 +0500

    fix .env

--- tengrinews/settings.py
+++ tengrinews/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 BASE_URL = "https://tengri-news-clone.onrender.com"
@@ -153,7 +153,7 @@ AUTH_USER_MODEL = "auth.User"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
